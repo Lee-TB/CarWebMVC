e028adeb292ebbfba562a85f9ff2a58ec9f86f21
Tommy Tran <tblyduc2412@gmail.com>
2024-07-14 16:17:38 +0700

    add VehicleImage Domain Model

---
+++ Data/Migrations/20240712092956_AddVehicleModelImages.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarWebMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVehicleModelImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VehicleImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    VehicleModelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VehicleImage_VehicleModels_VehicleModelId",
+                        column: x => x.VehicleModelId,
+                        principalTable: "VehicleModels",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleImage_VehicleModelId",
+                table: "VehicleImage",
+                column: "VehicleModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehicleImage");
+        }
+    }
+}
